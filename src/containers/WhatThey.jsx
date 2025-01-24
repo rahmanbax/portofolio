998e6a10f6f53d1ecc830587630f99e620477b7f
rahmanbax <115434374+rahmanbax@users.noreply.github.com>
2025-01-24 10:57:29 +0700

    Update WhatThey.jsx

--- src/containers/WhatThey.jsx
+++ src/containers/WhatThey.jsx
@@ -8,8 +8,8 @@ const WhatThey = () => {
     <div className="mt-20 lg:mt-40 mx-6 sm:mx-10 lg:mx-20 xl:mx-40">
       <h2 className="font-semibold text-xl lg:text-[28px]">What they say</h2>
       <div className="mt-6 lg:mt-12 flex flex-col lg:flex-row gap-4 lg:gap-8">
-        <Testimonial img={faiz} say="anjayani" name="Sianjg" job="Tukang ngocok" />
-        <Testimonial img={nadhif} say="mbot" name="Sijmbt" job="Citer gt" />
+        <Testimonial img={faiz} say="Testing" name="Faiz" job="Partner" />
+        <Testimonial img={nadhif} say="Testing 2" name="Nadhif" job="Partner" />
       </div>
     </div>
   );
